267a93b011db248cf8061da124cd3e344ff71c7d
Елена Денисова <E-denisova@yandex.ru>
2022-02-03 17:43:59 +0300

    INITIAL COMMIT

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int startBalance = 155;
+        int depositAmount = 544;
+        int bonusAmount = 0;
+
+        if(depositAmount >= 1000)
+            bonusAmount = depositAmount / 100;
+
+        int finalBalance = startBalance + depositAmount + bonusAmount;
+
+        System.out.println("Конечный баланс: " + finalBalance);
+        System.out.println("Бонус: " + (bonusAmount > 0 ? bonusAmount : "Как-нибудь в другой раз, жадина"));
+    }
+}
